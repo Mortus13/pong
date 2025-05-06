137fbc577e0a5409186b1649cd2272629a49ada7
ChverkoGleb <chverkogleb@bk.ru>
2025-05-06 14:30:30 +0700

    Исправил баг предыдущего коммита

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,14 @@ import { IoAdapter } from '@nestjs/platform-socket.io';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: 'https://pong-n66q.onrender.com'
+    origin: true,
+    credentials: true
   });
   // Настройка WebSocket
   app.useWebSocketAdapter(new IoAdapter(app));
 
   // Статические файлы
-  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.useStaticAssets(join(__dirname, '..', 'public'), {prefix: '/'});
   app.setBaseViewsDir(join(__dirname, '..', 'public'));
   app.setViewEngine('html');
 
